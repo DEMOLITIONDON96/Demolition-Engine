d0d22d59d2eeda230632bd802324165e69fb8665
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-03-31 14:23:06 -0500

    Create shaders_example.lua

---
+++ docs/shaders_example.lua
@@ -0,0 +1,6 @@
+function onCreatePost()
+    addEffect1('game', 0.005); --This is Chromatic Abbrevation
+    addEffect2('game', 0.1, 0.1, 0.1); --D&B Shaders because i know some people are gonna be asking for this
+    addEffect3('game', false); --Scanline effect :)
+    addEffect4('game', 0.0, true, true, true); --VCR effect, go nuts
+end
