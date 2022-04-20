2ddb943a53d6fcdb29c8cc5936bce8ad9bc48e89
Matt 3 <88756260+RonHereLMAO@users.noreply.github.com>
2022-04-20 16:06:32 -0500

    Tabi Real??

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -114,7 +114,7 @@ class FreeplayState extends MusicBeatState
 		for (i in 0...songs.length)
 		{
 			var songText:Alphabet = new Alphabet(0, (70 * i) + 30, songs[i].songName, true, false);
-			songText.isMenuItem = true;
+			songText.isMenuItemCenter = true;
 			songText.targetY = i;
 			grpSongs.add(songText);
 
