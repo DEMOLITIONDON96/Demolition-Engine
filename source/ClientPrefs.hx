8458501545fb56ccb86328f5f5af289fd9e5f6f8
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-04-20 09:17:18 -0500

    Score settings shit

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -25,6 +25,7 @@ class ClientPrefs {
 	public static var camZooms:Bool = true;
 	public static var hideHud:Bool = false;
 	public static var noteOffset:Int = 0;
+	public static var simplifiedScore:Bool = false;
 	public static var arrowHSV:Array<Array<Int>> = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
 	public static var imagesPersist:Bool = false;
 	public static var ghostTapping:Bool = true;
@@ -117,6 +118,7 @@ class ClientPrefs {
 		FlxG.save.data.camMove = camMove;
 		FlxG.save.data.showFPS = showFPS;
 		FlxG.save.data.flashing = flashing;
+		FlxB.save.data.simplifiedScore = simplifiedScore;
 		FlxG.save.data.globalAntialiasing = globalAntialiasing;
 		FlxG.save.data.noteSplashes = noteSplashes;
 		FlxG.save.data.lowQuality = lowQuality;
@@ -182,6 +184,9 @@ class ClientPrefs {
 		}
 		if(FlxG.save.data.laneTransparency != null) {
 			laneTransparency = FlxG.save.data.laneTransparency;
+		}
+		if(FlxG.save.data.simplifiedScore != null) {
+			simplifiedScore = FlxG.save.data.simplifiedScore;
 		}
 		if(FlxG.save.data.showFPS != null) {
 			showFPS = FlxG.save.data.showFPS;
