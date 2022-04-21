bc06c8f506e45edd517031a6d78c20596bf9f539
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-04-21 09:57:26 -0500

    fixing week 6 shaders rn

--- assets/preload/data/roses/woah shaders.lua
+++ assets/preload/data/roses/woah shaders.lua
@@ -2,8 +2,8 @@ function onCreatePost()
     addEffect1('camGame', 0.00500);
     addEffect1('camHUD', 0.00500);
     addEffect1('camOther', 0.00500);
-    addEffect4('camGame', 0, true, true, true);
-    addEffect4('camHUD', 0.3, true, true, true);
-    addEffect4('camOther', 0.3, true, true, true);
+    addEffect5('camGame', 0, true, true, true);
+    addEffect5('camHUD', 0.3, true, true, true);
+    addEffect5('camOther', 0.3, true, true, true);
 end
 --OMG SHADERS :000000000000000
