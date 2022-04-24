2f913092f815bb05b28ddc3276ffe6123c9012d5
Matt <88756260+matt-sudo-jpg@users.noreply.github.com>
2022-04-23 19:31:41 -0500

    some one dm me and said they ship me and demo.. note im a minor

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -2131,7 +2131,6 @@ class FunkinLua {
 		return PlayState.instance.isDead ? GameOverSubstate.instance : PlayState.instance;
 	}
 	static inline var CLENSE:String = "
-	os.execute = nil;
 	package.loaded.os.execute = nil;
 
 	os.getenv = nil;
