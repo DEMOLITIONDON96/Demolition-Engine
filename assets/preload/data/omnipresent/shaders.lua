c4ab3279d72ff0dc58e191d553273b16e8fef99b
Matt 3 <88756260+RonHereLMAO@users.noreply.github.com>
2022-04-24 23:19:47 -0500

    biggest shit i ever done and im not even done
    CHARACTERS LEFT
    TAKI
    ACE
    AFLAC
    AND HEX
    
    THEN FUCKING ALL THE BG
    AND MODCHARTS

---
+++ assets/preload/data/omnipresent/shaders.lua
@@ -0,0 +1,8 @@
+function onCreatePost()
+    addEffect1('camGame', 0.00500);
+    addEffect1('hud', 0.00500);
+    addEffect1('other', 0.00500);
+    addEffect5('camGame', 0, true, true, true);
+    addEffect5('hud', 0.3, true, true, true);
+    addEffect5('other', 0.3, true, true, true);
+end
