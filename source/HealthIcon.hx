a731e05f54be3bded49a1a42eb3069c09a06c8c8
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-05-04 10:05:12 -0500

    Just realized this code fucks up icons

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -56,9 +56,9 @@ class HealthIcon extends FlxSprite
 				this.char = char;
 			} else {
 				loadGraphic(file); //Load stupidly first for getting the file size
-				loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr
-				iconOffsets[0] = (width - 150) / 2;
-				iconOffsets[1] = (width - 150) / 2;
+				loadGraphic(file, true, Math.floor(width / 3), Math.floor(height)); //Then load it fr
+				iconOffsets[0] = (width - 150) / 3;
+				iconOffsets[1] = (width - 150) / 3;
 				updateHitbox();
 
 				animation.add(char, [0, 1], 0, false, isPlayer);
