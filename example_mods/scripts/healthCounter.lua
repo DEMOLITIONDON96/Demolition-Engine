3889926cbed40d47bcf27960019fa0988412eae6
CharlesCat <62410083+SonicProYT@users.noreply.github.com>
2022-05-04 20:59:17 -0400

    i dont even care anymore

--- example_mods/scripts/healthCounter.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-	makeLuaText('counter',math.floor(getProperty('health') * 500 / 10) .. '%',0,950,634)
-	setTextSize('counter', 25)
-	addLuaText('counter')
-end
-
-function onUpdate()
-	setTextString('counter',math.floor(getProperty('health') * 500 / 10) .. '%')
-end
