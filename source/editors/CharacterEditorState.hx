50d0efe69bb256bffdb028501bfe218baaa9970b
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-05-14 14:15:01 +0200

    Fixed the application name to the character that is editing

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -113,7 +113,7 @@ class CharacterEditorState extends MusicBeatState
 		});
 		changeBGbutton.cameras = [camMenu];
 		
-		Application.current.window.title = "Friday Night Funkin': Demolition Engine - Character Editor - Editing: " + daAnim;
+		Application.current.window.title = "Friday Night Funkin': Demolition Engine - Character Editor - Editing: " + char;
 		
 		loadChar(!daAnim.startsWith('bf'), false);
 
