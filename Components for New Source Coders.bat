cc38dc07de72512bc861b932bf32d2085f6ea93f
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-10-14 11:04:04 -0500

    Create Components for New Source Coders.bat

---
+++ Components for New Source Coders.bat
@@ -0,0 +1,22 @@
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+haxelib update flixel
+haxelib install flixel
+haxelib install flixel-addons
+haxelib install flixel-ui
+haxelib install hscript
+haxelib git tentools https://github.com/TentaRJ/tentools.git
+haxelib git systools https://github.com/haya3218/systools
+haxelib run lime rebuild systools windows
+haxelib install newgrounds
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
+haxelib git linc_luajit https://github.com/AndreiRudenko/linc_luajit
+haxelib git window_mod_flx https://github.com/DuskieWhy/window_mod_flx
+lime test windows
